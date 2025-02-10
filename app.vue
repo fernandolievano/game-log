d53177df66a3c009a88a63dc8f38bac56c98cf6b
fernandolievano <fernandolievan@gmail.com>
2025-02-10 20:04:40 -0300

    feat: add Tailwind CSS integration and update app layout

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <main class="bg-gray-800 text-white w-full h-screen">
+    <h1 class="text-center text-2xl">Hello There!</h1>
+  </main>
 </template>
