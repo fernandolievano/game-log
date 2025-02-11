4ec67db5919e5d7f182bfd054d9321ef0bc05072
fernandolievano <fernandolievan@gmail.com>
2025-02-11 00:18:42 -0300

    feat: add OAuthButton component with Google and GitHub icons

---
+++ components/auth/OAuthButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="w-full max-w-[220px] py-2 px-4  flex items-center justify-start gap-2 border rounded cursor-pointer transition-all text-black bg-white hover:bg-black hover:text-white hover:border-transparent dark:text-white dark:bg-black dark:hover:bg-white dark:hover:text-black hover:scale-110">
+    <span class="w-6">
+      <component :is="selectedComponent" v-if="selectedComponent"></component>
+    </span>
+    Sign up with {{ providerName }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+import IconGitHub from '@/components/svg/IconGitHub.vue';
+import IconGoogle from '@/components/svg/IconGoogle.vue';
+
+const props = defineProps<{
+  provider: 'github | google';
+}>();
+
+const providerName = computed(() => ({
+  google: 'Google',
+  github: 'GitHub',
+}[props.provider] || 'Undefined'));
+
+const componentsMap = {
+  github: IconGitHub,
+  google: IconGoogle
+};
+
+const selectedComponent = computed(() => componentsMap[props.provider] || null);
+</script>
