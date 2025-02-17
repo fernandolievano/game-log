908841a2d5f6a2716ee0a0110c2d73fd60807f8f
fernandolievano <fernandolievan@gmail.com>
2025-02-17 00:57:27 -0300

    feat: add pinia module for state management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
+  modules: ['@pinia/nuxt'],
   runtimeConfig: {
     public: {
       supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
