76485c75c254183723a2b5f51819d65c7e2eca6f
fernandolievano <fernandolievan@gmail.com>
2025-03-25 05:08:11 -0300

    feat: add loading skeleton for games while fetching data

--- stores/steam.ts
+++ stores/steam.ts
@@ -4,16 +4,20 @@ import type { SteamPlayer, SteamGame } from '@/interfaces/steam';
 export const useSteamStore = defineStore('steam', {
   state: () => ({
     player: null as SteamPlayer | null,
+    playerLoading: true,
     games: [] as SteamGame[],
+    gameLoading: true,
     game_count: 0
   }),
   actions: {
     setPlayerSummary(player: SteamPlayer) {
       this.player = player;
+      this.playerLoading = false;
     },
     setOwnedGames(games: SteamGame[], game_count: number) {
       this.games = games;
       this.game_count = game_count;
+      this.gameLoading = false;
     }
   }
 });
