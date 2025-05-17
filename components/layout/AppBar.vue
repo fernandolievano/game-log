a38eff31a71e838974db68c9dd7436f4e5907b05
fernandolievano <fernandolievan@gmail.com>
2025-05-17 19:21:19 -0300

    test: unit tests for AppBar

--- components/layout/AppBar.vue
+++ components/layout/AppBar.vue
@@ -2,20 +2,22 @@
   <div class="fixed top-0 left-0 w-full py-2 px-4 xl:rounded-xl h-20 z-50">
     <div class="flex justify-between items-center h-full w-full gap-2 relative">
       <!-- menu actions -->
-      <AppButtonBar ref="menuButtonRef" class="xl:hidden border-none bg-white dark:bg-black" @click="openMenu">
+      <AppButtonBar ref="menuButtonRef" data-test-id="menu-button" class="xl:hidden border-none bg-white dark:bg-black"
+        @click="openMenu">
         <Menu />
       </AppButtonBar>
 
       <div class="w-fit bg-white dark:bg-black py-2 px-4 rounded-full">
-        <nuxt-link to="/" class="font-mono text-2xl xl:text-3xl font-black tracking-wider my-0 py-0"
+        <nuxt-link to="/" data-test-id="logo-link"
+          class="font-mono text-2xl xl:text-3xl font-black tracking-wider my-0 py-0"
           @click="scrollUp">GameLog</nuxt-link>
       </div>
       <!-- menu actions -->
 
       <!-- theme actions -->
-      <AppButtonBar class="ml-auto bg-white dark:bg-black" aria-label="Change theme" title="Change theme"
-        @click="changeTheme">
-        <AppLoading v-if="!isMounted" />
+      <AppButtonBar class="ml-auto bg-white dark:bg-black" aria-label="Change theme" data-test-id="theme-button"
+        title="Change theme" @click="changeTheme">
+        <AppLoading v-if="!isMounted" data-test-id="loading-theme-button" />
         <span v-else>
           <Sun v-if="theme === 'dark'" />
           <Moon v-else />
